ead251ee7fd56f59fb26068b1e9116d3bc4d9d57
trandangntd <dangntdbsaf200067@fpt.edu.vn>
2022-04-24 17:28:37 +0700

    first commit

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
+
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -16,6 +17,7 @@ class CategoryType extends AbstractType
             ->add('catname', TextType::class)
         ;
     }
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
